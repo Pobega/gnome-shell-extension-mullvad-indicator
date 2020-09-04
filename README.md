5354563c0c8ad7ce51ffb03457b01e216d9b2c4d
Michael Pobega <pobega@gmail.com>
2020-09-04 12:37:26 -0400

    Big overhaul, prettier UI

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mullvad-gnome-indicator
+
+Special thanks to [LinxGem33](https://github.com/LinxGem33) whose [IP Finder](https://extensions.gnome.org/extension/2983/ip-finder/) layout code I relied on heavily.
--- extension.js
+++ extension.js
@@ -1,3 +1,4 @@
+const Clutter = imports.gi.Clutter;
 const GObject = imports.gi.GObject;
 const Gio = imports.gi.Gio;
 const Soup = imports.gi.Soup;
@@ -11,14 +12,16 @@ const PopupMenu = imports.ui.popupMenu;
 
 const API_URL = 'https://am.i.mullvad.net/json';
 
-const connectedIcon = 'mullvad-connected-symbolic';
-const disconnectedIcon = 'mullvad-disconnected-symbolic';
+const ICON_CONNECTED = 'mullvad-connected-symbolic';
+const ICON_DISCONNECTED = 'mullvad-disconnected-symbolic';
 
-let mullvadIndicator;
-
-function _log(msg) {
-    global.log(`[AmIMullvad] ${msg}`);
-}
+const DEFAULT_DATA = {
+    server: { name: _("Server"), text: ''},
+    country: { name: _("Country"), text: ''},
+    city: { name: _("City"), text: ''},
+    ip: { name: _("IP Address"), text: _('')},
+    type: { name: _("VPN Type"), text: ''},
+};
 
 const MullvadIndicator = GObject.registerClass({
     GTypeName: 'MullvadIndicator',
@@ -27,44 +30,79 @@ const MullvadIndicator = GObject.registerClass({
     _init() {
         super._init(0);
 
-        // Our status variables
-        this._statusConnected = false;
-        this._statusIpAddr = '';
-        this._statusServer = '';
+        this._connected = false;
+        // We use JSON here to 'clone' from our default Object
+        this._connStatus = JSON.parse(JSON.stringify(DEFAULT_DATA));
 
-        // Our taskbar icon
+        // Taskbar icon
         this._icon = new St.Icon({
             style_class: 'system-status-icon',
         });
-        this._updateIcon(disconnectedIcon);
-
+        this._updateIcon(ICON_DISCONNECTED);
         this.add_child(this._icon);
+        // End taskbar icon
 
-        this._conn_status = new St.Label({text: 'Checking...'});
-        let _conn_status = new PopupMenu.PopupMenuItem(
-            'Mullvad:',
-            {reactive: false}
-        );
-        _conn_status.add_child(this._conn_status);
-        this.menu.addMenuItem(_conn_status);
+        // Popup menu
+        let popupMenu = new PopupMenu.PopupMenuSection();
+        this.menu.box.style = "padding: 16px;";
+        let parentContainer = new St.BoxLayout({
+            x_align: Clutter.ActorAlign.FILL,
+            x_expand: true,
+            style: "min-width:240px; padding-bottom: 10px;"
+        });
+        // End popup menu
 
-        this._ip_addr = new St.Label({text: ''});
-        let _ip_addr = new PopupMenu.PopupMenuItem(
-            'IP Address:',
-            {reactive: false}
-        );
-        _ip_addr.add_child(this._ip_addr);
-        this.menu.addMenuItem(_ip_addr);
+        this.vpnInfoBox = new St.BoxLayout({
+            style_class: 'ip-info-box',
+            vertical: true ,
+            x_align: Clutter.ActorAlign.CENTER,
+        });
+        parentContainer.add_actor(this.vpnInfoBox);
+        popupMenu.actor.add(parentContainer);
+        this.menu.addMenuItem(popupMenu);
 
-        this._server = new St.Label({text: ''});
-        let _server = new PopupMenu.PopupMenuItem(
-            'Server:',
-            {reactive: false}
-        );
-        _server.add_child(this._server);
-        this.menu.addMenuItem(_server);
+        // Settings button
+        let buttonBox = new St.BoxLayout();
+        this._settingsIcon = new St.Icon({
+            icon_name: 'emblem-system-symbolic',
+            style_class: 'popup-menu-icon'
+        });
+        this._settingsButton = new St.Button({
+            child: this._settingsIcon,
+            style_class: 'button'
+        });
+        this._settingsButton.connect('clicked',  ()=> Util.spawnCommandLine('gnome-extensions prefs amimullvad@pobega.github.com'));
+        buttonBox.add_actor(this._settingsButton);
+        // End settings button
 
+        // Refresh button
+        this._refreshIcon = new St.Icon({
+            icon_name: 'view-refresh-symbolic',
+            style_class: 'popup-menu-icon'
+        });
+        this._refreshButton = new St.Button({
+            child: this._refreshIcon,
+            x_expand: true,
+            x_align: Clutter.ActorAlign.END,
+            style_class: 'button'
+        });
+        this._refreshButton.connect('clicked',  ()=> {
+            this._forceUpdate(100);
+        });
+        buttonBox.add_actor(this._refreshButton);
+        // End refresh button
+
+        popupMenu.actor.add(parentContainer);
+        popupMenu.actor.add_actor(buttonBox);
+        this.menu.addMenuItem(popupMenu);
+
+        Main.panel.addToStatusArea('AmIMullvad', this, 1);
         this._refresh();
+    }
+
+    _forceUpdate() {
+        this._connStatus = JSON.parse(JSON.stringify(DEFAULT_DATA));
+        this._fetchConnectionInfo();
     }
 
     _updateIcon(relative_path) {
@@ -83,6 +121,7 @@ const MullvadIndicator = GObject.registerClass({
     }
 
     _fetchConnectionInfo() {
+        global.log('mullvad - fetching conn info');
         let _httpSession = new Soup.Session();
         let message = Soup.Message.new('GET', API_URL);
         // Fake CURL to prevent 403
@@ -96,26 +135,93 @@ const MullvadIndicator = GObject.registerClass({
 
     _checkIfStatusChanged(api_response) {
         // Only update if our status has changed
-        if (this._statusConnected !== api_response.mullvad_exit_ip ||
-            this._statusIpAddr !== api_response.ip ||
-            this._statusServer !== api_response.mullvad_exit_ip_hostname) {
-            this._statusConnected = api_response.mullvad_exit_ip;
-            this._statusIpAddr = api_response.ip;
-            this._statusServer = api_response.mullvad_exit_ip_hostname;
+        if (this._connected !== api_response.mullvad_exit_ip ||
+            this._connStatus.ip.text !== api_response.ip ||
+            this._connStatus.server.text !== api_response.mullvad_exit_ip_hostname) {
+            this._connected = api_response.mullvad_exit_ip;
+            this._connStatus.ip.text = api_response.ip;
+            this._connStatus.server.text = api_response.mullvad_exit_ip_hostname;
+            this._connStatus.city.text = api_response.city;
+            this._connStatus.country.text = api_response.country;
+            this._connStatus.type.text = api_response.mullvad_server_type;
             this._updateGui();
+        } else {
         }
     }
 
     _updateGui() {
-        if (this._statusConnected === true) {
-            this._conn_status.set_text('Connected');
-            this._updateIcon(connectedIcon);
+        this.vpnInfoBox.destroy_all_children();
+
+        let vpnInfoRow = new St.BoxLayout();
+        this.vpnInfoBox.add_actor(vpnInfoRow);
+
+        let label = new St.Label({
+            style_class: this._connected ? 'vpn-info-vpn-on' : 'vpn-info-vpn-off',
+            text: _("Mullvad") + ': ',
+            x_align: Clutter.ActorAlign.START,
+            y_align: Clutter.ActorAlign.START,
+            y_expand: false,
+        });
+        vpnInfoRow.add_actor(label);
+
+        let vpnLabel = new St.Label({
+            style_class: this._connected ? 'ip-info-vpn-on' : 'ip-info-vpn-off',
+            text: this._connected ? 'Connected' : 'Disconnected',
+            x_align: Clutter.ActorAlign.FILL,
+            y_align: Clutter.ActorAlign.START,
+            x_expand: true,
+            y_expand: false,
+        });
+        vpnInfoRow.add_actor(vpnLabel);
+
+        this.vpnInfoBox.add_actor(new PopupMenu.PopupSeparatorMenuItem());
+
+        let vpnIcon = new St.Icon({
+            icon_name: this._connected ? 'security-high-symbolic' : 'security-low-symbolic',
+            style_class: this._connected ? 'popup-menu-icon vpn-info-vpn-on' : 'popup-menu-icon vpn-info-vpn-off',
+            x_align: Clutter.ActorAlign.END,
+            x_expand: true,
+        });
+        vpnInfoRow.add_actor(vpnIcon);
+
+        if (this._connected === true) {
+            this._updateIcon(ICON_CONNECTED);
         } else {
-            this._conn_status.set_text('Disconnected');
-            this._updateIcon(disconnectedIcon);
+            this._updateIcon(ICON_DISCONNECTED);
+            return;
         }
-        this._ip_addr.set_text(this._statusIpAddr || '');
-        this._server.set_text(this._statusServer || '');
+
+        for(let key in DEFAULT_DATA){
+            if(this._connStatus[key]){
+                let vpnInfoRow = new St.BoxLayout();
+                this.vpnInfoBox.add_actor(vpnInfoRow);
+
+                let label = new St.Label({
+                    style_class: 'vpn-info-key',
+                    text: _(DEFAULT_DATA[key].name) + ': ',
+                    x_align: Clutter.ActorAlign.FILL,
+                    y_align: Clutter.ActorAlign.CENTER,
+                    y_expand: true,
+                });
+                vpnInfoRow.add_actor(label);
+
+                let infoLabel = new St.Label({
+                    style_class: 'vpn-info-value',
+                    text: this._connStatus[key].text || '',
+                    x_align: Clutter.ActorAlign.FILL,
+                    y_align: Clutter.ActorAlign.CENTER,
+                    x_expand: true,
+                    y_expand: true,
+                });
+                let dataLabelBtn = new St.Button({
+                    child: infoLabel,
+                });
+                dataLabelBtn.connect('button-press-event', () => {
+                    Clipboard.set_text(CLIPBOARD_TYPE, dataLabelBtn.child.text);
+                });
+                vpnInfoRow.add_actor(dataLabelBtn);
+            }
+        }
     }
 
     stop() {
@@ -129,12 +235,13 @@ const MullvadIndicator = GObject.registerClass({
 function init() {
 }
 
+let _amIMullvad;
+
 function enable() {
-    mullvadIndicator = new MullvadIndicator();
-    Main.panel.addToStatusArea('AmIMullvad', mullvadIndicator, 1);
+    _amIMullvad = new MullvadIndicator();
 }
 
 function disable() {
-    mullvadIndicator.stop();
-    mullvadIndicator.destroy();
+    _amIMullvad.stop();
+    _amIMullvad.destroy();
 }
