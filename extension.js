35f9e1fc2ca480b98f072ea76523f42883780afa
Michael Pobega <pobega@gmail.com>
2020-09-04 05:58:28 -0400

    ESlinted
    .eslintrc taken from upstream's official gjs repository:
    https://gitlab.gnome.org/GNOME/gjs/-/blob/master/.eslintrc.yml

--- extension.js
+++ extension.js
@@ -16,15 +16,15 @@ const disconnectedIcon = 'mullvad-disconnected-symbolic';
 
 let mullvadIndicator;
 
-function log(msg) {
-    global.log('[AmIMullvad] ' + msg);
+function _log(msg) {
+    global.log(`[AmIMullvad] ${msg}`);
 }
 
 const MullvadIndicator = GObject.registerClass({
-    GTypeName: 'MullvadIndicator'
+    GTypeName: 'MullvadIndicator',
 }, class MullvadIndicator extends PanelMenu.Button {
 
-    _init () {
+    _init() {
         super._init(0);
 
         // Our status variables
@@ -34,32 +34,32 @@ const MullvadIndicator = GObject.registerClass({
 
         // Our taskbar icon
         this._icon = new St.Icon({
-            style_class : 'system-status-icon',
+            style_class: 'system-status-icon',
         });
         this._updateIcon(disconnectedIcon);
 
         this.add_child(this._icon);
 
-        this._conn_status = new St.Label({ text : 'Checking...' });
+        this._conn_status = new St.Label({text: 'Checking...'});
         let _conn_status = new PopupMenu.PopupMenuItem(
             'Mullvad:',
-            { reactive : false }
+            {reactive: false}
         );
         _conn_status.add_child(this._conn_status);
         this.menu.addMenuItem(_conn_status);
 
-        this._ip_addr = new St.Label({ text : '' });
+        this._ip_addr = new St.Label({text: ''});
         let _ip_addr = new PopupMenu.PopupMenuItem(
             'IP Address:',
-            { reactive : false }
+            {reactive: false}
         );
         _ip_addr.add_child(this._ip_addr);
         this.menu.addMenuItem(_ip_addr);
 
-        this._server = new St.Label({ text : '' });
+        this._server = new St.Label({text: ''});
         let _server = new PopupMenu.PopupMenuItem(
             'Server:',
-            { reactive : false }
+            {reactive: false}
         );
         _server.add_child(this._server);
         this.menu.addMenuItem(_server);
@@ -67,28 +67,28 @@ const MullvadIndicator = GObject.registerClass({
         this._refresh();
     }
 
-    _updateIcon (relative_path) {
+    _updateIcon(relative_path) {
         this._icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/${relative_path}.svg`);
     }
 
-    _refresh () {
+    _refresh() {
         this._fetchConnectionInfo();
         if (this._timeout) {
             Mainloop.source_remove(this._timeout);
             this._timeout = null;
         }
-        this._timeout = Mainloop.timeout_add_seconds(10, function() {
-            this._refresh()
+        this._timeout = Mainloop.timeout_add_seconds(10, function () {
+            this._refresh();
         }.bind(this));
     }
 
-    _fetchConnectionInfo () {
+    _fetchConnectionInfo() {
         let _httpSession = new Soup.Session();
         let message = Soup.Message.new('GET', API_URL);
         // Fake CURL to prevent 403
         message.request_headers.append('User-Agent', 'curl/7.68.0');
         message.request_headers.append('Accept', '*/*');
-        _httpSession.queue_message(message, function(session, message) {
+        _httpSession.queue_message(message, function (session, message) {
             let response = JSON.parse(JSON.stringify(message.response_body.data));
             this._checkIfStatusChanged(JSON.parse(response));
         }.bind(this));
@@ -96,29 +96,29 @@ const MullvadIndicator = GObject.registerClass({
 
     _checkIfStatusChanged(api_response) {
         // Only update if our status has changed
-        if (this._statusConnected != api_response.mullvad_exit_ip ||
-            this._statusIpAddr != api_response.ip ||
-            this._statusServer != api_response.mullvad_exit_ip_hostname) {
-          this._statusConnected = api_response.mullvad_exit_ip;
-          this._statusIpAddr = api_response.ip;
-          this._statusServer = api_response.mullvad_exit_ip_hostname;
-          this._updateGui();
+        if (this._statusConnected !== api_response.mullvad_exit_ip ||
+            this._statusIpAddr !== api_response.ip ||
+            this._statusServer !== api_response.mullvad_exit_ip_hostname) {
+            this._statusConnected = api_response.mullvad_exit_ip;
+            this._statusIpAddr = api_response.ip;
+            this._statusServer = api_response.mullvad_exit_ip_hostname;
+            this._updateGui();
         }
     }
 
     _updateGui() {
         if (this._statusConnected === true) {
-          this._conn_status.set_text('Connected');
-          this._updateIcon(connectedIcon);
+            this._conn_status.set_text('Connected');
+            this._updateIcon(connectedIcon);
         } else {
-          this._conn_status.set_text('Disconnected');
-          this._updateIcon(disconnectedIcon);
+            this._conn_status.set_text('Disconnected');
+            this._updateIcon(disconnectedIcon);
         }
         this._ip_addr.set_text(this.statusIpAddr || '');
         this._server.set_text(this.statusServer || '');
     }
 
-    stop () {
+    stop() {
         if (this._timeout)
             Mainloop.source_remove(this._timeout);
         this._timeout = undefined;
