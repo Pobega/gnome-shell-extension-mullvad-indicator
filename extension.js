ab132e61854948d916795813b3db4ff7a67761bc
Michael Pobega <pobega@gmail.com>
2020-09-04 05:46:28 -0400

    Adhere to Gjs style guidelines
    https://gitlab.gnome.org/GNOME/gjs/-/blob/master/doc/Style_Guide.md

--- extension.js
+++ extension.js
@@ -1,143 +1,140 @@
-const St = imports.gi.St;
-const Lang = imports.lang;
-const Main = imports.ui.main;
-const Mainloop = imports.mainloop;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
 const GObject = imports.gi.GObject;
 const Gio = imports.gi.Gio;
 const Soup = imports.gi.Soup;
+const St = imports.gi.St;
+
+const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
 
 const API_URL = 'https://am.i.mullvad.net/json';
 
-const ConnectedIcon = 'mullvad-connected-symbolic';
-const DisconnectedIcon = 'mullvad-disconnected-symbolic';
+const connectedIcon = 'mullvad-connected-symbolic';
+const disconnectedIcon = 'mullvad-disconnected-symbolic';
 
-let mvIndicator;
-
-const Me = imports.misc.extensionUtils.getCurrentExtension();
-
-let MVSTATUS;
-let MVIPADDR;
-let MVSERVER;
+let mullvadIndicator;
 
 function log(msg) {
-  global.log('[AmIMullvad] ' + msg);
+    global.log('[AmIMullvad] ' + msg);
 }
 
 const MullvadIndicator = GObject.registerClass({
-  GTypeName: 'MullvadIndicator'
+    GTypeName: 'MullvadIndicator'
 }, class MullvadIndicator extends PanelMenu.Button {
 
-  _init () {
-    super._init(0);
+    _init () {
+        super._init(0);
 
-    this.icon = new St.Icon({
-      style_class : 'system-status-icon',
-    });
-    this.icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/${DisconnectedIcon}.svg`);
+        // Our status variables
+        this._statusConnected = false;
+        this._statusIpAddr = '';
+        this._statusServer = '';
 
-    this.add_child(this.icon);
+        // Our taskbar icon
+        this._icon = new St.Icon({
+            style_class : 'system-status-icon',
+        });
+        this._updateIcon(disconnectedIcon);
 
-    this.conn_status = new St.Label({ text : 'Checking...' });
-    let conn_status = new PopupMenu.PopupMenuItem(
-      'Mullvad:',
-      { reactive : false }
-    );
-    conn_status.add_child(this.conn_status);
-    this.menu.addMenuItem(conn_status);
+        this.add_child(this._icon);
 
-    this.ip_addr = new St.Label({ text : '' });
-    let ip_addr = new PopupMenu.PopupMenuItem(
-      'IP Address:',
-      { reactive : false }
-    );
-    ip_addr.add_child(this.ip_addr);
-    this.menu.addMenuItem(ip_addr);
+        this._conn_status = new St.Label({ text : 'Checking...' });
+        let _conn_status = new PopupMenu.PopupMenuItem(
+            'Mullvad:',
+            { reactive : false }
+        );
+        _conn_status.add_child(this._conn_status);
+        this.menu.addMenuItem(_conn_status);
 
-    this.server = new St.Label({ text : '' });
-    let server = new PopupMenu.PopupMenuItem(
-      'Server:',
-      { reactive : false }
-    );
-    server.add_child(this.server);
-    this.menu.addMenuItem(server);
+        this._ip_addr = new St.Label({ text : '' });
+        let _ip_addr = new PopupMenu.PopupMenuItem(
+            'IP Address:',
+            { reactive : false }
+        );
+        _ip_addr.add_child(this._ip_addr);
+        this.menu.addMenuItem(_ip_addr);
 
-    this._refresh();
-  }
+        this._server = new St.Label({ text : '' });
+        let _server = new PopupMenu.PopupMenuItem(
+            'Server:',
+            { reactive : false }
+        );
+        _server.add_child(this._server);
+        this.menu.addMenuItem(_server);
 
-  _refresh () {
-    this._get_status();
-    if (this._timeout) {
-      Mainloop.source_remove(this._timeout);
-      this._timeout = null;
+        this._refresh();
     }
-    this._timeout = Mainloop.timeout_add_seconds(10, Lang.bind(this, this._refresh));
-  }
 
-  _get_status () {
-    let _httpSession = new Soup.Session();
-    let message = Soup.Message.new('GET', API_URL);
-    message.request_headers.append('User-Agent', 'curl/7.55.1');  // Fake CURL to prevent 403
-    message.request_headers.append('Accept', '*/*');  // Fake CURL to prevent 403
-    /* Don't want to use Lang.bind but can't figure out how to pass 'this'
-    _httpSession.queue_message(message, function(session, message) {
-      let response = JSON.parse(JSON.stringify(message.response_body.data));
-      this._check_if_status_changed(response);
-    });
-    */
-    _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
-      let response = JSON.parse(JSON.stringify(message.response_body.data));
-      this._check_if_status_changed(JSON.parse(response));
-    }));
-  }
-
-  _check_if_status_changed(api_response) {
-    // Only update if our status has changed
-    log("in");
-    if (MVSTATUS != api_response.mullvad_exit_ip ||
-        MVIPADDR != api_response.ip ||
-        MVSERVER != api_response.mullvad_exit_ip_hostname) {
-      MVSTATUS = api_response.mullvad_exit_ip;
-      MVIPADDR = api_response.ip;
-      MVSERVER = api_response.mullvad_exit_ip_hostname;
-      this._update();
+    _updateIcon (relative_path) {
+        this._icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/${relative_path}.svg`);
     }
-  }
 
-  _update() {
-    if (MVSTATUS === true) {
-      this.conn_status.set_text('Connected');
-      this.icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/${ConnectedIcon}.svg`);
-    } else {
-      log("Disconnected");
-      this.conn_status.set_text('Disconnected');
-      this.icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/${DisconnectedIcon}.svg`);
+    _refresh () {
+        this._fetchConnectionInfo();
+        if (this._timeout) {
+            Mainloop.source_remove(this._timeout);
+            this._timeout = null;
+        }
+        this._timeout = Mainloop.timeout_add_seconds(10, function() {
+            this._refresh()
+        }.bind(this));
     }
-    this.ip_addr.set_text(MVIPADDR || '');
-    this.server.set_text(MVSERVER || '');
-  }
 
-  stop () {
-  if (this._timeout)
-    Mainloop.source_remove(this._timeout);
-  this._timeout = undefined;
-  }
+    _fetchConnectionInfo () {
+        let _httpSession = new Soup.Session();
+        let message = Soup.Message.new('GET', API_URL);
+        // Fake CURL to prevent 403
+        message.request_headers.append('User-Agent', 'curl/7.68.0');
+        message.request_headers.append('Accept', '*/*');
+        _httpSession.queue_message(message, function(session, message) {
+            let response = JSON.parse(JSON.stringify(message.response_body.data));
+            this._checkIfStatusChanged(JSON.parse(response));
+        }.bind(this));
+    }
+
+    _checkIfStatusChanged(api_response) {
+        // Only update if our status has changed
+        if (this._statusConnected != api_response.mullvad_exit_ip ||
+            this._statusIpAddr != api_response.ip ||
+            this._statusServer != api_response.mullvad_exit_ip_hostname) {
+          this._statusConnected = api_response.mullvad_exit_ip;
+          this._statusIpAddr = api_response.ip;
+          this._statusServer = api_response.mullvad_exit_ip_hostname;
+          this._updateGui();
+        }
+    }
+
+    _updateGui() {
+        if (this._statusConnected === true) {
+          this._conn_status.set_text('Connected');
+          this._updateIcon(connectedIcon);
+        } else {
+          this._conn_status.set_text('Disconnected');
+          this._updateIcon(disconnectedIcon);
+        }
+        this._ip_addr.set_text(this.statusIpAddr || '');
+        this._server.set_text(this.statusServer || '');
+    }
+
+    stop () {
+        if (this._timeout)
+            Mainloop.source_remove(this._timeout);
+        this._timeout = undefined;
+    }
 
 });
 
-function init()
-{
+function init() {
 }
 
-function enable()
-{
-  mvIndicator = new MullvadIndicator();
-  Main.panel.addToStatusArea('Mullvad Indicator', mvIndicator, 1);
+function enable() {
+    mullvadIndicator = new MullvadIndicator();
+    Main.panel.addToStatusArea('AmIMullvad', mullvadIndicator, 1);
 }
 
-function disable()
-{
-  mvIndicator.stop();
-  mvIndicator.destroy();
+function disable() {
+    mullvadIndicator.stop();
+    mullvadIndicator.destroy();
 }
