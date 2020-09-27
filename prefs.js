456e52d94a6b5b61beaf117acee1376fa5e5e3f4
Michael Pobega <pobega@gmail.com>
2020-09-27 01:59:05 -0400

    Toggle visibility on some preferenes based on current settings

--- prefs.js
+++ prefs.js
@@ -24,23 +24,29 @@ const MullvadIndicatorPrefsWidget = class {
     }
 
     _connectSignals() {
+        // Set some signals to toggle visibility of some preferences
+        this._toggleConnectButtonKindVisibility();
+        this._settings.connect(
+            'changed::show-connect-button',
+            _setting => {
+                this._toggleConnectButtonKindVisibility();
+            }
+        );
+
+        this._toggleSystemdServiceNameVisibility();
+        this._settings.connect(
+            'changed::connect-command-type',
+            _setting => {
+                this._toggleSystemdServiceNameVisibility();
+            }
+        );
+
+        // Bind preferences to their Gschema settings
         let spinButton = this._builder.get_object('refreshTimeSpinButton');
         spinButton.set_range(1, 9999);
         spinButton.set_increments(10, 10);
         spinButton.set_value(this._settings.get_int('refresh-time'));
         this._settings.bind('refresh-time', spinButton, 'value', Gio.SettingsBindFlags.DEFAULT);
-
-        this._settings.bind(
-            'connect-command-type',
-            this._builder.get_object('connectButtonKindComboBoxText'),
-            'active-id',
-            Gio.SettingsBindFlags.DEFAULT);
-
-        this._settings.bind(
-            'service-name',
-            this._builder.get_object('mullvadSystemdServiceNameTextbox'),
-            'text',
-            Gio.SettingsBindFlags.DEFAULT);
 
         this._settings.bind(
             'show-icon',
@@ -58,6 +64,18 @@ const MullvadIndicatorPrefsWidget = class {
             'show-connect-button',
             this._builder.get_object('showConnectButtonSwitch'),
             'active',
+            Gio.SettingsBindFlags.DEFAULT);
+
+        this._settings.bind(
+            'connect-command-type',
+            this._builder.get_object('connectButtonKindComboBoxText'),
+            'active-id',
+            Gio.SettingsBindFlags.DEFAULT);
+
+        this._settings.bind(
+            'service-name',
+            this._builder.get_object('mullvadSystemdServiceNameTextbox'),
+            'text',
             Gio.SettingsBindFlags.DEFAULT);
 
         this._settings.bind(
@@ -89,6 +107,33 @@ const MullvadIndicatorPrefsWidget = class {
             this._builder.get_object('showVpnTypeSwitch'),
             'active',
             Gio.SettingsBindFlags.DEFAULT);
+    }
+
+    // Functions to toggle the visibility of some preferences based on
+    // whether the functionality is enabled/disabled
+
+    // Toggle connection type dropdown (Mullvad app/Systemd) if functionality is disabled
+    _toggleConnectButtonKindVisibility() {
+        const connectButtonKindBox = this._builder.get_object('connectButtonKindBox');
+        const systemdServiceNameBox = this._builder.get_object('mullvadSystemdServiceName');
+        if (this._settings.get_boolean('show-connect-button')) {
+            connectButtonKindBox.show();
+            this._toggleSystemdServiceNameVisibility();
+        } else {
+            connectButtonKindBox.hide();
+            systemdServiceNameBox.hide();
+        }
+    }
+
+    // Toggles Systemd service name entry field if Systemd is not the currently selected type
+    _toggleSystemdServiceNameVisibility() {
+        const systemdServiceNameBox = this._builder.get_object('mullvadSystemdServiceName');
+        const connectCommandType = this._settings.get_string('connect-command-type');
+        if (connectCommandType == 'systemd') {
+            systemdServiceNameBox.show();
+        } else {
+            systemdServiceNameBox.hide();
+        }
     }
 
 };
