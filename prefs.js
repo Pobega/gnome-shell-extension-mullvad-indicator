0adc89c32ac017c0507e6aedfcfb7b9e1c8c288b
no-comma <82644961+no-comma@users.noreply.github.com>
2023-05-18 18:36:00 +0300

    Customizable title and subtitle

--- prefs.js
+++ prefs.js
@@ -3,13 +3,11 @@ const Gettext = imports.gettext.domain('mullvadindicator');
 const _ = Gettext.gettext;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Mullvad = Me.imports.mullvad;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 
-const Config = imports.misc.config;
-const shellVersion = parseFloat(Config.PACKAGE_VERSION);
 
-let preferences;
 
 class MullvadIndicatorPrefsWidget extends Adw.PreferencesPage {
     static {
@@ -26,9 +24,10 @@ class MullvadIndicatorPrefsWidget extends Adw.PreferencesPage {
         });
         this.add(group);
 
-        group.add(this._prefRow(_('Display indicator icon'), 'show-icon', new Gtk.Switch(), 'active'));
-        group.add(this._prefRow(_('Show in system menu'), 'show-menu', new Gtk.Switch(), 'active'));
-        group.add(this._prefRow(_('Use server location as system menu text'), 'show-server-text', new Gtk.Switch(), 'active'));
+        group.add(this._actionRow(_('Display indicator icon'), 'show-icon', new Gtk.Switch(), 'active'));
+        group.add(this._actionRow(_('Show in system menu'), 'show-menu', new Gtk.Switch(), 'active'));
+        group.add(this._comboRow(_('Title text'), 'title-text', [...Mullvad.statusItemNames(), _('Connected')]));
+        group.add(this._comboRow(_('Subtitle text'), 'subtitle-text', [...Mullvad.statusItemNames(), _('None')]));
 
         group = new Adw.PreferencesGroup({
             title: _('Refresh'),
@@ -39,21 +38,21 @@ class MullvadIndicatorPrefsWidget extends Adw.PreferencesPage {
         spinButton.set_range(1, 9999);
         spinButton.set_increments(10, 10);
         spinButton.set_value(this._settings.get_int('refresh-time'));
-        group.add(this._prefRow(_('Automatic refresh time (in seconds)'), 'refresh-time', spinButton, 'value'));
+        group.add(this._actionRow(_('Automatic refresh time (in seconds)'), 'refresh-time', spinButton, 'value'));
 
         group = new Adw.PreferencesGroup({
             title: _('Status'),
         });
         this.add(group);
 
-        group.add(this._prefRow(_('Show currently connected server'), 'show-server', new Gtk.Switch(), 'active'));
-        group.add(this._prefRow(_("Show currently connected server's country"), 'show-country', new Gtk.Switch(), 'active'));
-        group.add(this._prefRow(_("Show currently connected server's city"), 'show-city', new Gtk.Switch(), 'active'));
-        group.add(this._prefRow(_('Show your current IP address'), 'show-ip', new Gtk.Switch(), 'active'));
-        group.add(this._prefRow(_('Show your VPN type (WireGuard/OpenVPN)'), 'show-type', new Gtk.Switch(), 'active'));
+        group.add(this._actionRow(_('Show currently connected server'), 'show-server', new Gtk.Switch(), 'active'));
+        group.add(this._actionRow(_("Show currently connected server's country"), 'show-country', new Gtk.Switch(), 'active'));
+        group.add(this._actionRow(_("Show currently connected server's city"), 'show-city', new Gtk.Switch(), 'active'));
+        group.add(this._actionRow(_('Show your current IP address'), 'show-ip', new Gtk.Switch(), 'active'));
+        group.add(this._actionRow(_('Show your VPN type (WireGuard/OpenVPN)'), 'show-type', new Gtk.Switch(), 'active'));
     }
 
-    _prefRow(title, key, object, property) {
+    _actionRow(title, key, object, property) {
         const row = new Adw.ActionRow({title: title});
 
         object.valign = Gtk.Align.CENTER;
@@ -61,6 +60,20 @@ class MullvadIndicatorPrefsWidget extends Adw.PreferencesPage {
         row.activatable_widget = object;
 
         this._settings.bind(key, object, property, Gio.SettingsBindFlags.DEFAULT);
+
+        return row;
+    }
+
+    _comboRow(title, key, list) {
+        const row = new Adw.ComboRow({
+            title: title,
+            model: new Gtk.StringList({
+                strings: list
+            }),
+            selected: this._settings.get_uint(key)
+        });
+
+        this._settings.bind(key, row, 'selected', Gio.SettingsBindFlags.DEFAULT);
 
         return row;
     }
