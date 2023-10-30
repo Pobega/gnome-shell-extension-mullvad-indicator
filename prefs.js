ecb819bbf1dcecd2201d29ca139889642ff4d84b
Michael Pobega <pobega@gmail.com>
2023-10-30 08:16:32 -0400

    gnome-45: update prefs.js per EGO review
    Make settings local to the window.

--- prefs.js
+++ prefs.js
@@ -8,7 +8,7 @@ import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Ex
 
 export default class MullvadIndicatorPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
-        this._settings = this.getSettings();
+        window._settings = this.getSettings();
 
         const page = new Adw.PreferencesPage();
 
@@ -17,10 +17,10 @@ export default class MullvadIndicatorPreferences extends ExtensionPreferences {
         });
         page.add(group);
 
-        group.add(this._actionRow(_('Display indicator icon'), 'show-icon', new Gtk.Switch(), 'active'));
-        group.add(this._actionRow(_('Show in system menu'), 'show-menu', new Gtk.Switch(), 'active'));
-        group.add(this._comboRow(_('Title text'), 'title-text', [...Mullvad.statusItemNames(), _('Connected')]));
-        group.add(this._comboRow(_('Subtitle text'), 'subtitle-text', [...Mullvad.statusItemNames(), _('None')]));
+        group.add(this._actionRow(_('Display indicator icon'), 'show-icon', new Gtk.Switch(), 'active', window._settings));
+        group.add(this._actionRow(_('Show in system menu'), 'show-menu', new Gtk.Switch(), 'active', window._settings));
+        group.add(this._comboRow(_('Title text'), 'title-text', [...Mullvad.statusItemNames(), _('Connected')], window._settings));
+        group.add(this._comboRow(_('Subtitle text'), 'subtitle-text', [...Mullvad.statusItemNames(), _('None')], window._settings));
 
         group = new Adw.PreferencesGroup({
             title: _('Refresh'),
@@ -30,45 +30,45 @@ export default class MullvadIndicatorPreferences extends ExtensionPreferences {
         let spinButton = new Gtk.SpinButton();
         spinButton.set_range(1, 9999);
         spinButton.set_increments(10, 10);
-        spinButton.set_value(this._settings.get_int('refresh-time'));
-        group.add(this._actionRow(_('Automatic refresh time (in seconds)'), 'refresh-time', spinButton, 'value'));
+        spinButton.set_value(window._settings.get_int('refresh-time'));
+        group.add(this._actionRow(_('Automatic refresh time (in seconds)'), 'refresh-time', spinButton, 'value', window._settings));
 
         group = new Adw.PreferencesGroup({
             title: _('Status'),
         });
         page.add(group);
 
-        group.add(this._actionRow(_('Show currently connected server'), 'show-server', new Gtk.Switch(), 'active'));
-        group.add(this._actionRow(_("Show currently connected server's country"), 'show-country', new Gtk.Switch(), 'active'));
-        group.add(this._actionRow(_("Show currently connected server's city"), 'show-city', new Gtk.Switch(), 'active'));
-        group.add(this._actionRow(_('Show your current IP address'), 'show-ip', new Gtk.Switch(), 'active'));
-        group.add(this._actionRow(_('Show your VPN type (WireGuard/OpenVPN)'), 'show-type', new Gtk.Switch(), 'active'));
+        group.add(this._actionRow(_('Show currently connected server'), 'show-server', new Gtk.Switch(), 'active', window._settings));
+        group.add(this._actionRow(_("Show currently connected server's country"), 'show-country', new Gtk.Switch(), 'active', window._settings));
+        group.add(this._actionRow(_("Show currently connected server's city"), 'show-city', new Gtk.Switch(), 'active', window._settings));
+        group.add(this._actionRow(_('Show your current IP address'), 'show-ip', new Gtk.Switch(), 'active', window._settings));
+        group.add(this._actionRow(_('Show your VPN type (WireGuard/OpenVPN)'), 'show-type', new Gtk.Switch(), 'active', window._settings));
 
         window.add(page);
     }
 
-    _actionRow(title, key, object, property) {
+    _actionRow(title, key, object, property, settings) {
         const row = new Adw.ActionRow({title: title});
 
         object.valign = Gtk.Align.CENTER;
         row.add_suffix(object);
         row.activatable_widget = object;
 
-        this._settings.bind(key, object, property, Gio.SettingsBindFlags.DEFAULT);
+        settings.bind(key, object, property, Gio.SettingsBindFlags.DEFAULT);
 
         return row;
     }
 
-    _comboRow(title, key, list) {
+    _comboRow(title, key, list, settings) {
         const row = new Adw.ComboRow({
             title: title,
             model: new Gtk.StringList({
                 strings: list
             }),
-            selected: this._settings.get_uint(key)
+            selected: settings.get_uint(key)
         });
 
-        this._settings.bind(key, row, 'selected', Gio.SettingsBindFlags.DEFAULT);
+        settings.bind(key, row, 'selected', Gio.SettingsBindFlags.DEFAULT);
 
         return row;
     }
